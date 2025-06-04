844cad300860b5d73b23ac9d5c42a2e92182ae92
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2025-06-04 08:34:07 +0100

    feature/TP_0003 Implemented solution to question three - sum of digits and digital root

--- src/main/java/com/assessment/techieplanet/programmingandalgorithm/QuestionThree.java
+++ src/main/java/com/assessment/techieplanet/programmingandalgorithm/QuestionThree.java
@@ -1,4 +1,26 @@
 package com.assessment.techieplanet.programmingandalgorithm;
 
 public class QuestionThree {
+
+    public static void main(String[] args) {
+        String input = "1234445123444512344451234445123444512344451234445";
+        int result = sumDigits(input);
+        System.out.println("Sum of digits -> " + result);
+        int digitalRoot = digitalRoot(input);
+        System.out.println("Digital root -> " + digitalRoot);
+    }
+
+    public static int sumDigits(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        int firstDigit = s.charAt(0) - '0';
+        return firstDigit + sumDigits(s.substring(1));
+    }
+
+    public static int digitalRoot(String s) {
+        int sum = sumDigits(s);
+        while (sum >= 10) {sum = sumDigits(String.valueOf(sum));}
+        return sum;
+    }
 }
